f7283ea04c69fdb5efa49270845f5df976a3e3db
PoonamGhutukade <47653686+PoonamGhutukade@users.noreply.github.com>
2019-06-29 15:29:43 +0530

    Extra in BL

---
+++ ExtraWork/impor files/functions_file.py
@@ -0,0 +1,12 @@
+def print_data(para):
+    print("Hello!!!",para)
+    return
+
+def sum_num(a,b):
+    c = a + b
+    return c
+
+def load_libraries(para):
+    print("All libraries loaded here", para)
+    return
+    
